6fdcceb7d328201a5d6fff11b9afb0012a1f3ab9
Yannick Molinghen <yannick.molinghen@ulb.be>
2026-02-12 10:23:19 +0100

    CI

--- src/stub_gen.rs
+++ src/stub_gen.rs
@@ -2,6 +2,8 @@ use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
     lle::bindings::stub_info()?.generate()?;
+    // The __init__ file is manually written, so remove that one.
+    std::fs::remove_file("python/lle/__init__.pyi")?;
     println!("Generated Python stubs successfully.");
     Ok(())
 }
