8f9d61a45947dcd245f586371834aaf1fb3cd8b4
hugocharels <hugocharels@hotmail.com>
2026-05-14 17:01:55 +0200

    feat(solver): port _internal SAT helpers (model, variables, grid, types)

---
+++ python/lle/solver/_internal/__init__.py
@@ -0,0 +1,14 @@
+from ._model import SATModel
+from ._variables import VariableFactory
+from ._grid import all_positions, get_neighbors, is_within_bounds
+from ._types import agents_from_world, laser_sources_from_world
+
+__all__ = [
+    "SATModel",
+    "VariableFactory",
+    "all_positions",
+    "get_neighbors",
+    "is_within_bounds",
+    "agents_from_world",
+    "laser_sources_from_world",
+]
