c55d2ba244a518b2c3f672fe1cf176af34b1acee
Yannick Molinghen <yannick.molinghen@ulb.be>
2026-07-13 12:28:38 +0200

    Fix no-chain validation test signature

--- python/tests/solver/test_no_chain.py
+++ python/tests/solver/test_no_chain.py
@@ -3,7 +3,7 @@ from lle import World
 from lle.characterization import WorldCharacterizer
 
 
-def test_compute_shortest_path_without_chain_rejects_length_below_2(length: int):
+def test_compute_shortest_path_without_chain_rejects_length_below_2():
     wc = WorldCharacterizer(World.level(1), t_max=10)
     for length in [1, 0, -1]:
         with pytest.raises(ValueError):
